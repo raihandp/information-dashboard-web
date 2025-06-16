a0701357320ac1a4c4ef5ec8c415064b05a2ffdd
RayhanHidayat <rayhananhidayat@gmail.com>
2025-06-16 15:11:26 +0700

    chore: Initialize seeder

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,16 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import { env } from '$env/dynamic/private';
+import { seeder } from '$lib/server/db/seeder/seeder';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = mysql.createPool(env.DATABASE_URL);
 
 export const db = drizzle(client, { casing: 'snake_case' });
+
+async function main() {
+	seeder();
+}
+
+main();

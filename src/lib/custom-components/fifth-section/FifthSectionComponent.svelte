314a3dc0d01e97eac8a9fc5064cd010cb512e2a1
RayhanHidayat <rayhananhidayat@gmail.com>
2025-06-23 09:44:42 +0700

    chore: progress on banner for social media and marquee

--- src/lib/custom-components/fifth-section/FifthSectionComponent.svelte
+++ src/lib/custom-components/fifth-section/FifthSectionComponent.svelte
@@ -1,26 +1,44 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { AspectRatio } from '$lib/components/ui/aspect-ratio/index.js';
+	import { Marquee } from '@selemondev/svelte-marquee';
+	import '@selemondev/svelte-marquee/dist/style.css';
+	import Globe from '@lucide/svelte/icons/globe';
+	import Instagram from '@lucide/svelte/icons/instagram';
+	import Youtube from '@lucide/svelte/icons/youtube';
 </script>
 
 <div class="mt-2 mb-5 w-full">
 	<div
-		class="mr-5 ml-5 h-13 rounded-lg bg-emerald-500/30 p-5 drop-shadow-lg backdrop-blur-sm"
-	></div>
+		class="mr-5 ml-5 rounded-lg bg-orange-500 p-3 font-semibold text-white drop-shadow-lg backdrop-blur-sm"
+	>
+		<Marquee
+			direction="left"
+			fade={true}
+			reverse={false}
+			pauseOnHover={false}
+			class="[--duration:20s]"
+			innerClassName="w-full"
+		>
+			<h3>Content 1 ontent 1 ontent 1 ontent 1 ontent 1</h3>
+			<h3>Content 2</h3>
+			<h3>Content 3</h3>
+		</Marquee>
+	</div>
 	<div class="mt-3 flex items-center justify-around">
 		<div class="flex flex-row items-center justify-center">
-			<!-- <img width="30px" height="30px" src={''} alt="Instagram Logo" /> -->
-			<h6 class="ms-1 scroll-m-20 text-lg font-semibold tracking-tight text-nowrap">@fikupnvj</h6>
+			<Instagram size="30px" />
+			<h6 class="scroll-m-20 ps-1 text-lg font-semibold tracking-tight text-nowrap">@fikupnvj</h6>
 		</div>
 		<div class="flex flex-row items-center justify-center">
-			<!-- <img width="30px" height="30px" src={''} alt="Web Logo" /> -->
-			<h6 class="ms-1 scroll-m-20 text-lg font-semibold tracking-tight text-nowrap">
+			<Globe size="30px" />
+			<h6 class="scroll-m-20 ps-1 text-lg font-semibold tracking-tight text-nowrap">
 				new-fik.upnvj.ac.id
 			</h6>
 		</div>
 		<div class="flex flex-row items-center justify-center">
-			<!-- <img width="30px" height="30px" src={''} alt="Youtube Logo" /> -->
-			<h6 class="ms-1 scroll-m-20 text-lg font-semibold tracking-tight text-nowrap">@fikupnvj</h6>
+			<Youtube size="30px" />
+			<h6 class="scroll-m-20 ps-1 text-lg font-semibold tracking-tight text-nowrap">@fikupnvj</h6>
 		</div>
 	</div>
 </div>

7e62b6e12284064be521a2c889a4b5a62a0df4ef
raihandp <rdpringgodigdo@gmail.com>
2025-07-15 15:51:37 +0700

    buat sidebar v2

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -93,42 +93,4 @@
    <Sidebar.GroupContent />
   </Sidebar.Group>
  </Sidebar.Content>
-     <Sidebar.Footer>
-      <Sidebar.Menu>
-        <Sidebar.MenuItem>
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              {#snippet child({ props })}
-                <Sidebar.MenuButton
-                  {...props}
-                  class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-                >
-                  Username
-                  <ChevronUp class="ml-auto" />
-                </Sidebar.MenuButton>
-              {/snippet}
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content
-              side="top"
-              class="w-(--bits-dropdown-menu-anchor-width)"
-            >
-              <DropdownMenu.Item>
-                <span>Account</span>
-              </DropdownMenu.Item>
-              <DropdownMenu.Item>
-                <span>Billing</span>
-              </DropdownMenu.Item>
-              <DropdownMenu.Item>
-                <span>Sign out</span>
-              </DropdownMenu.Item>
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
-        </Sidebar.MenuItem>
-      </Sidebar.Menu>
-    </Sidebar.Footer>
-  </Sidebar.Root>
-  <Sidebar.Inset>
-    <header class="flex h-12 items-center justify-between px-4">
-      <Sidebar.Trigger />
-    </header>
-  </Sidebar.Inset>
+</Sidebar.Root>

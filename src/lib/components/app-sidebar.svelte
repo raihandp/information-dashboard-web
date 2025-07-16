a1275c5f3e8869bddec6c654e8102882c8745677
raihandp <rdpringgodigdo@gmail.com>
2025-07-16 13:49:55 +0700

    template header & footer sidebar, add icon

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,52 +1,64 @@
 <script lang="ts">
- import CalendarIcon from "@lucide/svelte/icons/calendar";
- import HouseIcon from "@lucide/svelte/icons/house";
- import InboxIcon from "@lucide/svelte/icons/inbox";
- import SearchIcon from "@lucide/svelte/icons/search";
- import SettingsIcon from "@lucide/svelte/icons/settings";
  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-
- import PlusIcon from "@lucide/svelte/icons/plus";
+ import { 
+  HouseIcon, 
+  SquarePen, 
+  Tally1, 
+  Tally2, 
+  Tally3, 
+  SquareMousePointer, 
+  TvMinimalPlay, 
+  RectangleEllipsis,
+  ChevronDown,
+  ChevronUp,
+ } from '@lucide/svelte';
  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-  
+
+
  // Menu items.
  const items = [
+  // {
+  //  title: "Halaman Depan",
+  //  url: "/",
+  //  icon: HouseIcon,
+  // },
+
   {
    title: "Edit Banner 1",
    url: "/dashboard/banner-1",
-  //  icon: HouseIcon,
+   icon: Tally1,
   },
   {
    title: "Edit Banner 2",
    url: "/dashboard/banner-2",
-  //  icon: HouseIcon,
+   icon: Tally2,
   },
   {
    title: "Edit Banner 3",
    url: "/dashboard/banner-3",
-  //  icon: HouseIcon,
+   icon: Tally3,
   },
   {
    title: "Edit Grup Tombol",
    url: "/dashboard/edit-button",
-  //  icon: HouseIcon,
+   icon: SquareMousePointer,
   },
   {
    title: "Edit Slideshow",
    url: "/dashboard/slideshow",
-  //  icon: HouseIcon,
+   icon: TvMinimalPlay,
   },
   {
    title: "Edit Teks Berjalan",
    url: "/dashboard/running-text",
-  //  icon: HouseIcon,
+   icon: RectangleEllipsis,
   },
  ];
 </script>
  
 <Sidebar.Root>
   <Sidebar.Header>
-    <!-- <Sidebar.Menu>
+    <Sidebar.Menu>
       <Sidebar.MenuItem>
         <DropdownMenu.Root>
           <DropdownMenu.Trigger>
@@ -67,31 +79,61 @@
           </DropdownMenu.Content>
         </DropdownMenu.Root>
       </Sidebar.MenuItem>
-    </Sidebar.Menu> -->
+    </Sidebar.Menu>
   </Sidebar.Header>
 
- <Sidebar.Content>
-  <Sidebar.Group>
-   <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
-   <Sidebar.GroupContent>
-    <Sidebar.Menu>
-     {#each items as item (item.title)}
-      <Sidebar.MenuItem>
-       <Sidebar.MenuButton>
-        {#snippet child({ props })}
-         <a href={item.url} {...props}>
-          <!-- <item.icon /> -->
-          <span>{item.title}</span>
-         </a>
-        {/snippet}
-       </Sidebar.MenuButton>
-      </Sidebar.MenuItem>
-     {/each}
-    </Sidebar.Menu>
-   </Sidebar.GroupContent>
-  </Sidebar.Group>
- </Sidebar.Content>
+  <Sidebar.Content>
+    <Sidebar.Group>
+    <Sidebar.GroupContent>
+      <Sidebar.Menu>
+      {#each items as item (item.title)}
+        <Sidebar.MenuItem>
+        <Sidebar.MenuButton>
+          {#snippet child({ props })}
+          <a href={item.url} {...props}>
+            <item.icon />
+            <span>{item.title}</span>
+          </a>
+          {/snippet}
+        </Sidebar.MenuButton>
+        </Sidebar.MenuItem>
+      {/each}
+      </Sidebar.Menu>
+    </Sidebar.GroupContent>
+    </Sidebar.Group>
+  </Sidebar.Content>
 
-    <Sidebar.Footer>
-    </Sidebar.Footer>
+  <Sidebar.Footer>
+    <Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            {#snippet child({ props })}
+              <Sidebar.MenuButton
+                {...props}
+                class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                Username
+                <ChevronUp class="ml-auto" />
+              </Sidebar.MenuButton>
+            {/snippet}
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content
+            side="top"
+            class="w-(--bits-dropdown-menu-anchor-width)"
+          >
+            <DropdownMenu.Item>
+              <span>Account</span>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+              <span>Billing</span>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+              <span>Sign out</span>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.Footer>
 </Sidebar.Root>

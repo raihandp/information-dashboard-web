7ea0232d0bd70e9f8eef6c2eb5b43491417c2809
raihandp <rdpringgodigdo@gmail.com>
2025-07-16 09:51:10 +0700

    update button sidebar

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -7,36 +7,65 @@
  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 
  import PlusIcon from "@lucide/svelte/icons/plus";
-//  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-
+ import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
   
  // Menu items.
  const items = [
   {
-   title: "Home",
+   title: "Edit Banner 1",
    url: "#",
-   icon: HouseIcon,
+  //  icon: HouseIcon,
   },
   {
-   title: "Inbox",
+   title: "Edit Banner 2",
    url: "#",
-   icon: InboxIcon,
+  //  icon: HouseIcon,
   },
   {
-   title: "Calendar",
+   title: "Edit Banner 3",
    url: "#",
-   icon: CalendarIcon,
+  //  icon: HouseIcon,
   },
   {
-   title: "Search",
+   title: "Edit Grup Tombol",
    url: "#",
-   icon: SearchIcon,
+  //  icon: HouseIcon,
   },
   {
-   title: "Settings",
+   title: "Edit Slideshow",
    url: "#",
-   icon: SettingsIcon,
+  //  icon: HouseIcon,
   },
+  {
+   title: "Edit Teks Berjalan",
+   url: "#",
+  //  icon: HouseIcon,
+  },
+  // {
+  //  title: "Home",
+  //  url: "#",
+  //  icon: HouseIcon,
+  // },
+  // {
+  //  title: "Inbox",
+  //  url: "#",
+  //  icon: InboxIcon,
+  // },
+  // {
+  //  title: "Calendar",
+  //  url: "#",
+  //  icon: CalendarIcon,
+  // },
+  // {
+  //  title: "Search",
+  //  url: "#",
+  //  icon: SearchIcon,
+  // },
+  // {
+  //  title: "Settings",
+  //  url: "#",
+  //  icon: SettingsIcon,
+  // },
  ];
 </script>
  
@@ -75,7 +104,7 @@
        <Sidebar.MenuButton>
         {#snippet child({ props })}
          <a href={item.url} {...props}>
-          <item.icon />
+          <!-- <item.icon /> -->
           <span>{item.title}</span>
          </a>
         {/snippet}
@@ -85,12 +114,46 @@
     </Sidebar.Menu>
    </Sidebar.GroupContent>
   </Sidebar.Group>
-  <Sidebar.Group>
+  
+  <!-- <Sidebar.Group>
    <Sidebar.GroupLabel>Projects</Sidebar.GroupLabel>
    <Sidebar.GroupAction title="Add Project">
     <PlusIcon /> <span class="sr-only">Add Project</span>
    </Sidebar.GroupAction>
    <Sidebar.GroupContent />
-  </Sidebar.Group>
+  </Sidebar.Group> -->
  </Sidebar.Content>
+    <!-- <Sidebar.Footer>
+      <Sidebar.Menu>
+        <Sidebar.MenuItem>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              {#snippet child({ props })}
+                <Sidebar.MenuButton
+                  {...props}
+                  class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                  Username
+                  <ChevronUp class="ml-auto" />
+                </Sidebar.MenuButton>
+              {/snippet}
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content
+              side="top"
+              class="w-(--bits-dropdown-menu-anchor-width)"
+            >
+              <DropdownMenu.Item>
+                <span>Account</span>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item>
+                <span>Billing</span>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item>
+                <span>Sign out</span>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </Sidebar.MenuItem>
+      </Sidebar.Menu>
+    </Sidebar.Footer> -->
 </Sidebar.Root>
